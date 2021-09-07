fadb321abae65820028b6880a0ec2c23642656cf
Ivan Vilches Basaul <ivanvilches@MacBook-Air-de-Ivan.local>
2021-09-06 22:45:01 -0300

    add simple navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+const NavBar = () => {
+  return (
+    <nav class="topnav">
+      <ul>
+        <li>
+          <a href="#">Inicio</a>
+        </li>
+        <li>
+          <a href="#">Sobre nosotros</a>
+        </li>
+        <li>
+          <a href="#">Contacto</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
